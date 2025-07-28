e9d022dfdfe8114f580f6baecefb0c27259dc4e2
Gururaj Krishna <gururajkrishna106@gmail.com>
2025-07-28 09:24:55 -0700

    [Feature]: add Location model with name, city, coordinates, description, images, and link

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by Gururaj Krishna on 7/28/25.
+//
+
+import Foundation
+import MapKit
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
+
