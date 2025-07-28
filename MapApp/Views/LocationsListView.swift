467ad4406639f080df17fab38727658572afba03
Gururaj Krishna <gururajkrishna106@gmail.com>
2025-07-28 11:43:37 -0700

    [Feature]Created city name and picture

---
+++ MapApp/Views/LocationsListView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsListView.swift
+//  MapApp
+//
+//  Created by Gururaj Krishna on 7/28/25.
+//
+
+import SwiftUI
+
+struct LocationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsListView()
+}
