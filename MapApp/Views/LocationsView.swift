aab1aa921927a35c98c7936fdd355d9747fec130
Gururaj Krishna <gururajkrishna106@gmail.com>
2025-07-28 09:58:28 -0700

    [Feature] created a locationView which displays struct Location into the view and made the struct Identifible

---
+++ MapApp/Views/LocationsView.swift
@@ -0,0 +1,31 @@
+//
+//  LocationsView.swift
+//  MapApp
+//
+//  Created by Gururaj Krishna on 7/28/25.
+//
+
+import SwiftUI
+
+class LocationsViewModel: ObservableObject {
+    @Published var locations: [Location]
+    init() {
+        let locations = LocationsDataService.locations
+        self.locations = locations
+    }
+}
+struct LocationsView: View {
+    @StateObject private var vm = LocationsViewModel()
+    var body: some View {
+        List {
+            ForEach(vm.locations) {
+                Text($0.name)
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    LocationsView()
+}
